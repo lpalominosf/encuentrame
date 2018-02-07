53447b1515a0f31f2d39dfbf3133beac9f29451a
Lore Palominos <lorena.palominos@gmail.com>
2018-02-07 17:05:48 -0300

    trazar ruta ok

--- app.js
+++ app.js
@@ -1,36 +1,87 @@
-function initMap(){
-  var map = new google.maps.Map(document.getElementById("map"),{
-    zoom: 5,//zoom representa el nivel de profundidad de nuestro mapa, entre más zoom más localizado se verá.
-    center: {lat: -35.4316852, lng: -71.542969},//center contiene la longitud y latitud en que queremos que se muestre nuestro mapa
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
     mapTypeControl: false,
-    zoomControl: false,
-    streetViewControl: false
+    center: {lat: -35.4316852, lng: -71.542969},
+    zoom: 7
   });
 
-  function buscar(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);//getCurrentPosition permite al usuario obtener su ubicación actual, funcionExito se ejecuta solo cuando el usuario comparte su ubicación, mientras que funcionError se ejecuta cuando se produce un error en la geolocalización
-    }
+  new AutocompleteDirectionsHandler(map);
   }
 
-  document.getElementById("encuentrame").addEventListener("click", buscar);
-  var latitud, longitud;
+  function AutocompleteDirectionsHandler(map) {
+    this.map = map;
+    this.originPlaceId = null;
+    this.destinationPlaceId = null;
+    this.travelMode = 'WALKING';
+    var originInput = document.getElementById('origin-input');
+    var destinationInput = document.getElementById('destination-input');
+    var modeSelector = document.getElementById('mode-selector');
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setMap(map);
 
-  var funcionExito = function(position){//var funcionExito, con el que obtendremos nuestra latitud o longitud y además crearemos un marcador de nuestra ubicación.
-    latitud = position.coords.latitude;
-    longitud = position.coords.longitude;
+    var originAutocomplete = new google.maps.places.Autocomplete(
+      originInput, {placeIdOnly: true});
+    var destinationAutocomplete = new google.maps.places.Autocomplete(
+      destinationInput, {placeIdOnly: true});
 
-    var miUbicacion = new google.maps.Marker({
-      position: {lat:latitud, lng:longitud},
-      animation: google.maps.Animation.DROP,
-      map: map
+    this.setupClickListener('changemode-walking', 'WALKING');
+    this.setupClickListener('changemode-transit', 'TRANSIT');
+    this.setupClickListener('changemode-driving', 'DRIVING');
+
+    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  }
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+    var radioButton = document.getElementById(id);
+    var me = this;
+    radioButton.addEventListener('click', function() {
+      me.travelMode = mode;
+      me.route();
     });
+  };
 
-    map.setZoom(17);
-    map.setCenter({lat:latitud, lng:longitud});
-  }
+  AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
+          return;
+        }
+        if (mode === 'ORIG') {
+          me.originPlaceId = place.place_id;
+        } else {
+          me.destinationPlaceId = place.place_id;
+        }
+          me.route();
+        });
 
-  var funcionError = function(error){//funcionError con un mensaje para el usuario, en caso de que nuestra geolocalización falle.
-    alert("tenemos un problema con encontrar tu ubicación");
-  }
-}
+      };
+
+  AutocompleteDirectionsHandler.prototype.route = function() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+    var me = this;
+
+  this.directionsService.route({
+    origin: {'placeId': this.originPlaceId},
+    destination: {'placeId': this.destinationPlaceId},
+    travelMode: this.travelMode
+  }, function(response, status) {
+    if (status === 'OK') {
+      me.directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+};
