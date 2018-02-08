fe0ee2c70ce9038f6592f0a4d8e9d787033c9fa7
Lore Palominos <lorena.palominos@gmail.com>
2018-02-08 14:50:32 -0300

    cambio de estilos

--- app.js
+++ app.js
@@ -81,9 +81,6 @@ function initMap() {
     this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
     this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
 
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
   }
 
   /*
